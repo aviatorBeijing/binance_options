0b39e818947e253d7af74988b6d412a7ceb95415
Jun Ma <junma018@outlook.com>
2024-02-27 19:04:06 +0800

    dev

--- src/straddle.py
+++ src/straddle.py
@@ -4,10 +4,18 @@ import click
 
 from butils import DATADIR
 
+def _v(v): return float(v)
+def calc_straddle( ldata,rdata):
+    lbid,lask,l_bvol, l_avol = ldata['bid'],ldata['ask'],ldata['bidv'],ldata['askv']
+    rbid,rask,r_bvol, r_avol = rdata['bid'],rdata['ask'],rdata['bidv'],rdata['askv']
+    for v in [lbid,lask,l_bvol,l_avol, rbid,rask,r_bvol,r_avol]: v = _v(v)
+    print(type(lbid))
+    
+
 @click.command()
 @click.option('--left', help="left leg contract name")
 @click.option('--right')
-@click.option('--use_best', is_flag=True, default=False, help='Use best available market price, i.e., buy on the best offer price, instead of trade price.')
+@click.option('--use_best', is_flag=True, default=False, help='Use best available market price, i.e., buy on the best offer price, instead of mid price.')
 def main(left,right, use_best):
     ldata = None;rdata = None
     with open(f"{DATADIR}/{left.upper()}.json", 'r') as fh:
@@ -16,6 +24,14 @@ def main(left,right, use_best):
     with open(f"{DATADIR}/{right.upper()}.json", 'r') as fh:
         rdata = json.loads(fh.read())
         print( rdata )
+    
+    if not ldata:
+        raise Exception(f'*** {left.upper()} contract is not found in cached dir: {DATADIR}')
+    if not rdata: 
+        raise Exception(f'*** {right.upper()} contract is not found in cached dir: {DATADIR}')
+    
+    calc_straddle( ldata,rdata)
+
 
 if __name__ == '__main__':
     main()
