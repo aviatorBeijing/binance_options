6d0082ecfa29d67682829482a23a692a434aa3ef
Jun Ma <junma018@outlook.com>
2024-02-27 19:29:02 +0800

    dev

--- src/straddle.py
+++ src/straddle.py
@@ -6,12 +6,15 @@ from tabulate import tabulate
 from butils import DATADIR
 
 def _v(v): return float(v)
-def calc_straddle( ldata,rdata, strike_left,strike_right):
+def calc_straddle( ldata,rdata, strike_left,strike_right, vol):
+    fee = 5/10000 # Binance: taker 5/10000, maker 2/10000
     lbid,lask,l_bvol, l_avol = _v(ldata['bid']),_v(ldata['ask']),_v(ldata['bidv']),_v(ldata['askv'])
     rbid,rask,r_bvol, r_avol = _v(rdata['bid']),_v(rdata['ask']),_v(rdata['bidv']),_v(rdata['askv'])
     assert lask<rask, "Left leg has to be less than right leg (offer price, a.k.a. ask price)"
+    print(f'-- order volumes  (L): {vol}-contract, (R): {vol}-contract')
     print(f'-- strikes (L): ${strike_left}, (R): ${strike_right}')
     recs = []
+    
     for stock in range(40000,70000,1000): # at expiration
         gains = max(strike_left - stock,0)
         gains += max( stock - strike_right, 0)
@@ -21,15 +24,15 @@ def calc_straddle( ldata,rdata, strike_left,strike_right):
     df = pd.DataFrame.from_records( recs, columns=['spot','cost', 'gain', 'profit'])
 
     for col in ['cost','profit']:
-        df[col] = df[col].apply(lambda e: f"${e:-,.0f}")
+        df[col] = df[col].apply(lambda e: f"${e:,.0f}")
     print( tabulate(df, headers="keys"))
     
 
 @click.command()
 @click.option('--left', help="left leg contract name")
 @click.option('--right')
-@click.option('--use_best', is_flag=True, default=False, help='Use best available market price, i.e., buy on the best offer price, instead of mid price.')
-def main(left,right, use_best):
+@click.option('--vol', default=1, help="planned order volume, 1=1BTC contract")
+def main(left,right, vol):
     ldata = None;rdata = None
     with open(f"{DATADIR}/{left.upper()}.json", 'r') as fh:
         ldata = json.loads(fh.read())
@@ -46,7 +49,7 @@ def main(left,right, use_best):
     if not rdata: 
         raise Exception(f'*** {right.upper()} contract is not found in cached dir: {DATADIR}')
     
-    calc_straddle( ldata,rdata, strike_left,strike_right)
+    calc_straddle( ldata,rdata, strike_left,strike_right,vol)
 
 
 if __name__ == '__main__':
