34b6964595fb3b55a268679dbf2dc9ef4da7691c
Jun Ma <junma018@outlook.com>
2024-02-27 19:23:54 +0800

    dev

--- src/straddle.py
+++ src/straddle.py
@@ -20,7 +20,7 @@ def calc_straddle( ldata,rdata, strike_left,strike_right):
         recs += [ (stock, cost, gains, profits )]
     df = pd.DataFrame.from_records( recs, columns=['spot','cost', 'gain', 'profit'])
 
-    df.cost = df.cost.apply(lambda e: f"${e:,.2f}")
+    df.cost = df.cost.apply(lambda e: f"${e:,.0f}")
     print( tabulate(df, headers="keys"))
     
 
