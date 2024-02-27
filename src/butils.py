6970d8d2400061b321dc54aad7e4279e0a225276
Jun Ma <junma018@outlook.com>
2024-02-27 20:38:20 +0800

    dev

--- src/butils.py
+++ src/butils.py
@@ -2,5 +2,9 @@ import os
 
 DATADIR=os.getenv('USER_HOME','/home/ubuntu')+'/data/binance/options'
 if not os.path.exists( DATADIR):
-    os.makedirs( DATADIR )
+    try:
+        os.makedirs( DATADIR )
+    except Exception as e:
+        print('*** Make sure set the "USER_HOME" directory for temporary data storage!')
+
 print('-- data dir:', DATADIR)
