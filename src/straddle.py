9c58169982305903bab6b939a98b93ef6edb23b0
Jun Ma <junma018@outlook.com>
2024-02-27 19:31:46 +0800

    dev

--- src/straddle.py
+++ src/straddle.py
@@ -25,7 +25,7 @@ def calc_straddle( ldata,rdata, strike_left,strike_right, vol):
     df = pd.DataFrame.from_records( recs, columns=['spot','premium','fee','gain', 'profit'])
 
     for col in ['premium','profit','fee']:
-        df[col] = df[col].apply(lambda e: f"${e:,.0f}")
+        df[col] = df[col].apply(lambda e: f"${e:,.2f}")
     print( tabulate(df, headers="keys"))
     
 
