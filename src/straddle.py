079b5f3b7b4514dbda642b094bf9778da36ab2d1
Jun Ma <junma018@outlook.com>
2024-02-27 20:55:04 +0800

    dev

--- src/straddle.py
+++ src/straddle.py
@@ -49,12 +49,17 @@ def calc_straddle( ldata,rdata, strike_left,strike_right, vol):
 def _main(left,right, vol):
     ldata = None;rdata = None
     print("-- Contracts --")
-    with open(f"{DATADIR}/{left.upper()}.json", 'r') as fh:
-        ldata = json.loads(fh.read())
-        print( '\t',left, ldata )
-    with open(f"{DATADIR}/{right.upper()}.json", 'r') as fh:
-        rdata = json.loads(fh.read())
-        print( '\t',right, rdata )
+    try:
+        with open(f"{DATADIR}/{left.upper()}.json", 'r') as fh:
+            ldata = json.loads(fh.read())
+            print( '\t',left, ldata )
+        with open(f"{DATADIR}/{right.upper()}.json", 'r') as fh:
+            rdata = json.loads(fh.read())
+            print( '\t',right, rdata )
+    except FileNotFoundError as  e:
+        print('*** waiting for data ...')
+        time.sleep(5)
+        return 
     
     if not ldata:
         raise Exception(f'*** {left.upper()} contract is not found in cached dir: {DATADIR}')
