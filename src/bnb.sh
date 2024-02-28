84d8dec2ca903d235938282633870c6985536e16
Jun Ma <junma018@outlook.com>
2024-02-28 17:38:20 +0800

    dev

--- src/bnb.sh
+++ src/bnb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$PYTHON strategy/straddle.py --left BNB-240301-400-P --right BNB-240301-405-C --size=1
+$PYTHON strategy/straddle.py --left BNB-240301-400-P --right BNB-240301-405-C --size=1  --user_premium=12.5
