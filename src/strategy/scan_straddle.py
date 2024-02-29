9f8fcfe7da2973e9b5d85a6d9924b107b672e4cf
Jun Ma <junma018@outlook.com>
2024-02-29 15:48:30 +0800

    dev

--- src/strategy/scan_straddle.py
+++ src/strategy/scan_straddle.py
@@ -19,7 +19,7 @@ def _main( contracts,sz ):
     df['break_even_low'] = df.be_prices.apply(lambda e: e[0])
     df['break_even_high'] = df.be_prices.apply(lambda e: e[3])
     df['low_r'] = df.be_ratios.apply(lambda e: f"{(e[0]*100):.1f}%")
-    df['high_r'] = df.be_ratios.apply(lambda e: e[3])
+    df['high_r'] = df.be_ratios.apply(lambda e: f"{(e[3]*100):.1f}%")
     
     df.drop(['is_taker','be_prices','be_ratios'], inplace=True, axis=1)
     print( df )
