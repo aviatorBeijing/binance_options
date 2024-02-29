42079afdb5cc264c402c23ad13e175731b7fc98a
Jun Ma <junma018@outlook.com>
2024-02-29 15:32:00 +0800

    dev

--- src/strategy/scan_straddle.py
+++ src/strategy/scan_straddle.py
@@ -4,10 +4,7 @@ import pandas as pd
 
 from strategy.straddle import _main as calc_straddle 
 
-@click.command()
-@click.option('--contracts')
-@click.option('--sz', default=1.)
-def main( contracts,sz ):
+def _main( contracts,sz ):
     contracts = contracts.split(',')
     puts = list(filter(lambda s: s.endswith('-P'), contracts))
     calls = list(filter(lambda s: s.endswith('-C'), contracts))
@@ -19,6 +16,18 @@ def main( contracts,sz ):
             recs += [resp]
     df = pd.DataFrame.from_records( recs )
     print( df )
+
+@click.command()
+@click.option('--contracts')
+@click.option('--data', is_flag=True,defaut=False)
+@click.option('--sz', default=1.)
+def main(contracts, data, sz):
+    if data:
+        from ws_bcontract import _main as ws_connector
+        ws_connector( contracts,'ticker')
+    else:
+        _main(contracts,sz)
+
 
 if __name__ == '__main__':
     main()
