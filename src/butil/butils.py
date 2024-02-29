b63b25840d32dec74bc33dad4e4f21042dc1c6dc
Jun Ma <junma018@outlook.com>
2024-02-29 18:50:49 +0800

    dev

--- src/butil/butils.py
+++ src/butil/butils.py
@@ -66,9 +66,7 @@ def get_binance_index(contract)->tuple:
     if len(fd) == 1:
         fd = fd[0]
         with open(f"{INDICESDIR}/{fd}", 'r') as fh:
-            t,v = fh.read().split('\n')
-            
-            print('***', t, v)
+            t = fh.read();print('***', t)
             cachedt = datetime.datetime.fromisoformat( t )
             if (bjnow() - cachedt).seconds < 10: # less than 10 sec
                 return float(v)
