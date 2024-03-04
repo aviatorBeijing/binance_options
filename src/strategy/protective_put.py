03301b3fd85be35ed5fe4be582eb07f68701d00c
Jun Ma <junma018@outlook.com>
2024-03-04 21:26:03 +0800

    dev: add protective put

--- src/strategy/protective_put.py
+++ src/strategy/protective_put.py
@@ -38,6 +38,7 @@ def calc_profits_profile(spot_quantity, contract, cdata):
     print(f'\t     fee = ${fee:.2f}')
     print(f'\t    cost = ${cost:.2f}')
     print(f'\t     qty = {contract_quantity:.2f} contracts @ ${ask:.4f}/contract')
+    print(f'\t    spot = ${spot_price:.4f}')
     for price  in np.arange(low,high,step):
         contract_value = max(0, strike-price )*contract_quantity*nominal - cost
         spot_value = price * spot_quantity
