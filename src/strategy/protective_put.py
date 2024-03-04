2490f3fdf68b8200200452cfe03e7e0091f2a1e1
Jun Ma <junma018@outlook.com>
2024-03-04 21:34:23 +0800

    dev: add protective put

--- src/strategy/protective_put.py
+++ src/strategy/protective_put.py
@@ -47,6 +47,17 @@ def calc_profits_profile(spot_quantity, contract, cdata):
     df = pd.DataFrame.from_records( recs )
     df.columns = ['spot','protective','put_value','spot_value']
     df['all_loss'] = (df.put_value + cost).apply(lambda x: np.isclose(x,0.0))
+    first_idx = df['all_loss'].ne(True).idxmax()
+    if first_idx >0:
+        if first_idx<df.shape[0]-1:
+            df = df[:first_idx+1]
+        else:
+            print( df )
+            raise Exception(f"Price range might not be enough. No all_loss found.")
+    else:
+        print(df)
+        raise Exception(f"Price range might not be enough. No all_loss found.")
+
 
     print( df )
 
