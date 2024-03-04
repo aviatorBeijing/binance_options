71ae6110d09ce14f31a2072b3be4e046a20d5ad1
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-03-04 14:53:07 +0800

    dev

--- src/doge.sh
+++ src/doge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CRYPTO=ETH
-DT=240306
-PUT=3475
-CALL=3475
+CRYPTO=DOGE
+DT=240308
+PUT=0.169
+CALL=0.169
 
 $PYTHON strategy/straddle.py --left $CRYPTO-$DT-$PUT-P --right $CRYPTO-$DT-$CALL-C --size=1 
