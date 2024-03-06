1d2bbc1d259718c4419417f606ca8e567f2bc273
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-03-06 11:17:37 +0800

    .

--- src/btc.sh
+++ src/btc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DT=240306
-LEFT=BTC-$DT-64500-P
-RIGHT=BTC-$DT-65500-C
+LEFT=BTC-$DT-65000-P
+RIGHT=BTC-$DT-65000-C
 $PYTHON strategy/straddle.py --left $LEFT --right $RIGHT --size=0.1
