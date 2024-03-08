5ff28bca180ebfbfb2ebdb50d4454a3742fddec3
Jun Ma <junma018@outlook.com>
2024-03-08 12:02:33 +0800

    dev: add vol ratios

---
+++ src/sentiments/vol_ratios.py
@@ -0,0 +1,30 @@
+import datetime,os
+import pandas as pd
+import requests 
+import click
+
+def fetch_contracts(underlying):
+    endpoint='https://eapi.binance.com/eapi/v1/exchangeInfo'
+    resp = requests.get(endpoint)
+    if resp:
+        resp  = resp.json()
+        ts = resp['serverTime']
+        print('-- server time:', pd.Timestamp( datetime.datetime.fromtimestamp( int(float(ts)/1000)) ) )
+        rics = resp['optionSymbols']
+        df = pd.DataFrame.from_records( rics )
+        
+        df.expiryDate = df.expiryDate.apply(float).apply(lambda v:v/1000).apply(datetime.datetime.fromtimestamp).apply(pd.Timestamp)
+        df.strikePrice = df.strikePrice.apply(float)
+        df['tickSize'] = df['filters'].apply(lambda v: v[0]['tickSize'] )
+        #df['minQty'] = df['filters'].apply(lambda v: v[1]['minQty'])
+
+        df.drop(['filters','contractId','unit','id'], axis=1, inplace=True)
+        df = df.sort_values(['expiryDate','symbol','strikePrice'], ascending=True)
+        return df 
+    return pd.DataFrame()
+
+@click.command()
+@click.option('--underlying', default="BTC")
+def main(underlying,price,low,high,contract):
+    df = fetch_contracts( underlying )
+    print( df )
