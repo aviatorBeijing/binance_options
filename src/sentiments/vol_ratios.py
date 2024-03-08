3e022d6122ab5abff567cb4c07d2edfba6edaae3
Jun Ma <junma018@outlook.com>
2024-03-08 20:10:40 +0800

    dev: add vol ratios

--- src/sentiments/vol_ratios.py
+++ src/sentiments/vol_ratios.py
@@ -80,8 +80,8 @@ def main(underlying):
     ohlcs = binance_kline(f"{underlying.upper()}/USDT", '1d')
     ohlcs['rtn'] = ohlcs.close.pct_change().tail(100)
     vols = {
-        "7d":  ohlcs.rtn.rolling(7).apply(np.std).iloc[-1],
-        "30d": ohlcs.rtn.rolling(30).apply(np.std).iloc[-1],
+        "7d":  ohlcs.rtn.rolling(7).apply(np.std).mean(),
+        "30d": ohlcs.rtn.rolling(30).apply(np.std).mean(),
     }
 
     # Vols
