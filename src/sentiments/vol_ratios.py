ac4a3a7f15ad3d6c17824b44338e71420aed4402
Jun Ma <junma018@outlook.com>
2024-03-09 13:37:47 +0800

    dev: add vol

--- src/sentiments/vol_ratios.py
+++ src/sentiments/vol_ratios.py
@@ -91,6 +91,7 @@ def main(underlying):
         closeNd = ohlcs.close.dropna().pct_change()
         if n>1:
             closeNd = closeNd.rolling(n)
+        print( closeNd )
         d = closeNd.apply(lambda s: talib.EMA(s))
         sigma = d.iloc[-1]
         sigma *= np.sqrt(365/n)
