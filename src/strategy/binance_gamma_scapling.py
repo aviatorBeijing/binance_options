5ab7b0cee00a78f7dad66d4a8e99eb6f34f8a3fb
Jun Ma <junma018@outlook.com>
2024-03-13 19:30:03 +0800

    dev

--- src/strategy/binance_gamma_scapling.py
+++ src/strategy/binance_gamma_scapling.py
@@ -6,17 +6,23 @@ import numpy as np
 from ws_bcontract import _main as ws_connector, sync_fetch_ticker
 from butil.butils import DATADIR,get_binance_next_funding_rate,DEBUG
 from brisk.bfee import calc_fee
-from strategy.gamma_scalping import EuropeanOption, Asset
+from strategy.gamma_scalping import EuropeanOption, Asset, Spot 
+
+spot_positions = []
 
 def _main( contracts:list ):
+    global spot_positions
     for opt in contracts:
         delta = opt.greeks['delta']
         gamma = opt.greeks['gamma']
         vol = opt.greeks['impvol']
 
         opt.on_market_move()
+        print('    -- spots:', spot_positions)
     
 def _mp_main(contracts:str):
+    global spot_positions
+
     cts = []
     qty = 1.
     nominal = 1.
@@ -29,6 +35,7 @@ def _mp_main(contracts:str):
     print('-- initial spot:', init_spot)
     print('-- initial option delta:', total_option_deltas)
     print(f'-- need to upfront {"SHORT" if td>0 else "LONG" if td<0 else "STAY"} {abs(td)*nominal} share of {underlying}')
+    spot_positions += [ Spot(underlying,init_spot,total_option_deltas) ]
 
     while True:
         try:
