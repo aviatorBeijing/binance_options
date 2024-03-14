9662067861ae793a53195d6d58bd51628bceb5f0
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-03-14 11:55:07 +0800

    m

--- src/btc_straddle.sh
+++ src/btc_straddle.sh
@@ -3,10 +3,10 @@
 # can also use "./atms.sh" to find all avalialbe ATM options, and select two ATM options.
 
 CRYPTO=BTC
-STRIKE=71000
-DT=240313
+STRIKE=74000
+DT=240322
 
 PUT=$STRIKE
 CALL=$STRIKE
 
-$PYTHON strategy/straddle.py --left $CRYPTO-$DT-$PUT-P --right $CRYPTO-$DT-$CALL-C --size=0.01 --user_premium=24.55 
+$PYTHON strategy/straddle.py --left $CRYPTO-$DT-$PUT-P --right $CRYPTO-$DT-$CALL-C --size=0.01 # --user_premium=24.55 
