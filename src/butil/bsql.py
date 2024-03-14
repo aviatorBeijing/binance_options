9321f32ec774f10551305f6aff10bd1d07fce30f
Jun Ma <junma018@outlook.com>
2024-03-14 14:51:40 +0800

    dev

--- src/butil/bsql.py
+++ src/butil/bsql.py
@@ -45,7 +45,7 @@ def update_bidask_tbl( data:dict ):
         if k != 'contract':
             kv_pairs += [f'{k}={data[k]}']
     stmt = f'''
-INSERT INTO {bidask_greeks_tbl} ({keys}) VALUES ({vals}) ON CONFLICE contract
+INSERT INTO {bidask_greeks_tbl} ({keys}) VALUES ({','.join(vals)}) ON CONFLICE contract
 DO UPDATE SET {','.join(kv_pairs)};
 '''
     try:
