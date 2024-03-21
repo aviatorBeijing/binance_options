c374ccdfbd6f93c6aeb7654becda17d6ade0f0ed
Jun Ma <junma018@outlook.com>
2024-03-21 15:38:44 +0800

    dev

--- src/strategy/straddle.py
+++ src/strategy/straddle.py
@@ -70,8 +70,10 @@ def calc_straddle(  lcontract, rcontract,
     K = float(lcontract.split('-')[2])
     if lcontract.endswith('-C'):
         fairvol_bid = fair_call_vol(float(ldata["bid"]),S,K,Tl)
+        fairvol_ask = fair_call_vol(float(ldata["ask"]),S,K,Tl)
     elif lcontract.endswith('-P'):
         fairvol_bid = fair_put_vol(float(ldata["bid"]),S,K,Tl)
+        fairvol_ask = fair_put_vol(float(ldata["ask"]),S,K,Tl)
     print(f'  -- vols of {lcontract} (bid,ask,mark,fair): {(float(ldata["impvol_bid"])*100):.1f}% ({(fairvol_bid*100):.1f}%), {(float(ldata["impvol_ask"])*100):.1f}% ({(fairvol_ask*100):.1f}%), {(float(ldata["impvol"])*100):.1f}%')
 
     lfee = 0;rfee=0
