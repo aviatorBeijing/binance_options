cba58f8ce3b5b3454cad5d4b5a9150a6740d920c
Jun Ma <junma018@outlook.com>
2024-03-22 10:17:04 +0800

    dev

--- src/bbroker/place_order.py
+++ src/bbroker/place_order.py
@@ -23,7 +23,7 @@ bal = bal['info']['balances']
 bdf  = pd.DataFrame.from_records(bal)
 bdf['free'] = bdf['free'].apply(float)
 bdf['locked'] = bdf['locked'].apply(float)
-bdf['ttl'] =  bdf['free'] +  bdf['ttl']
+bdf['ttl'] =  bdf['free'] +  bdf['locked']
 bdf.sort_values('ttl', ascending=False, inplace=True)
 print( bdf )
 
