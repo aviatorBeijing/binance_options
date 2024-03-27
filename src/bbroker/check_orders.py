c1e9589ad2fc03b3a3bb2df437589b208385d1a5
Jun Ma <junma018@outlook.com>
2024-03-27 10:35:42 +0800

    dev

--- src/bbroker/check_orders.py
+++ src/bbroker/check_orders.py
@@ -5,14 +5,14 @@ from tabulate import tabulate
 from bbroker.settings import ex
 
 def orders_status(ids=[]):
-    tnow = datetime.datetime.utcnow().timestamp()*1000;tnow=int(tnow)
+    #tnow = datetime.datetime.utcnow().timestamp()*1000;tnow=int(tnow)
     if ids:
         pass 
     else: # all
         ods = ex.eapiPrivateGetOpenOrders()
         df = pd.DataFrame.from_records(ods)
-        df['dt'] = (tnow - df.updateTime.apply(int))/1000
-        df = df[['dt','status','orderId','symbol','side','price','avgPrice','quantity','executedQty','updateTime','source','clientOrderId','priceScale','quantityScale']]
+        #df['dt'] = (tnow - df.updateTime.apply(int))/1000
+        df = df[['status','orderId','symbol','side','price','avgPrice','quantity','executedQty','updateTime','source','clientOrderId','priceScale','quantityScale']]
         df['datetime'] = df.updateTime.apply(int).apply(lambda v: datetime.datetime.fromtimestamp(v/1000))
         df = df.sort_values('updateTime', ascending=False)
         print(tabulate(df,headers="keys"))
