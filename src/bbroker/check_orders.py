0d5bf3ae6bb3b4420fca244c9c107d9efe230286
Jun Ma <junma018@outlook.com>
2024-03-27 10:49:10 +0800

    dev

--- src/bbroker/check_orders.py
+++ src/bbroker/check_orders.py
@@ -32,7 +32,7 @@ def position_status():
     df['expiry'] = df.expiryDate.apply(int).apply(lambda v: datetime.datetime.fromtimestamp(v/1000))
     df = df.sort_values(['symbol','expiryDate'], ascending=False)
     print(tabulate(df,headers="keys"))
-    print('-- dollar gain: ', df.positionCost.sum()-(df.entryPrice*df.quantity).sum() )
+    print('-- dollar gain: ', df.positionCost.astype(float).sum()-(df.entryPrice.astype(float)*df.quantity.astype(float)).sum() )
 
 # tests
 if __name__ == '__main__':
