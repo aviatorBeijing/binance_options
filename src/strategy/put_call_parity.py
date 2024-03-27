78d5d0d8901a7256cb38c02901d86f4a1e0dbecf
Jun Ma <junma018@outlook.com>
2024-03-27 19:58:09 +0800

    dev

---
+++ src/strategy/put_call_parity.py
@@ -0,0 +1,65 @@
+import time
+import pandas as pd
+import click,time
+from tabulate import tabulate
+import numpy  as np
+from multiprocessing import Process
+
+from butil.bsql import fetch_bidask
+from butil.butils import get_maturity,get_binance_spot
+from ws_bcontract import _main as ws_connector
+
+def _multiprocess_main(contracts):
+    contracts = contracts.split(',')
+    cts = []
+    for c in contracts:
+        head = c[:-1]
+        if head not in cts:
+            cts += [ head ]
+    spot_symbol = cts[0].split('-')[0] +  '/USDT'
+
+    while True:
+        bid,ask = get_binance_spot( spot_symbol )
+        S = (bid+ask)*0.5
+        recs = []
+        for t in cts:
+            call = t + 'C'
+            put  = t + 'P' 
+            T = get_maturity( call )/365
+            K = float( t.split('-')[2] )
+            rf = 0
+            try:
+                r = fetch_bidask(call)
+                call_ask = C = r['ask'];call_bid = r['bid']
+                r = fetch_bidask(put)
+                put_ask = P = r['ask'];put_bid = r['bid']
+                PV = K * np.exp(rf*T)
+                # C + PV = P + S
+                # replicating
+                print('-- replicating call (buy put,long spot(or forward),short bond):', f'{call_ask:.2f}', f'{(put_ask + S - PV):.2f}' )
+                print('-- replicating  put (buy call, short spot(or forward), long bond):', f'{put_ask:.2f}', f'{(call_ask + PV -S):.2f}' )   
+            except AssertionError as e:
+                print('*** waiting for data')
+                time.sleep(5)
+                continue
+        df = pd.DataFrame.from_records(recs)
+        df = df[['contract','gamma','ask','last_trade','delta','theta','impvol']]
+        df.gamma = df.gamma.apply(float)
+        df.sort_values('gamma', ascending=False,inplace=True)
+        df = df.head(10)
+        print( '-- first 10:\n', tabulate(df, headers="keys") )
+        time.sleep(5)
+
+@click.command()
+@click.option('--contracts')
+def main(contracts):
+    conn = Process( target=ws_connector, args=(contracts, "ticker",) )
+    calc = Process( target=_multiprocess_main, args=(contracts,) )
+    conn.start()
+    calc.start()
+    
+    conn.join()
+    calc.join()
+
+if __name__ ==  '__main__':
+    main()
