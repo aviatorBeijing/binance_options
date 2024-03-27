ebe5bc491d91644129b67878795fc8985455cead
Jun Ma <junma018@outlook.com>
2024-03-27 10:36:08 +0800

    dev

--- src/bbroker/check_orders.py
+++ src/bbroker/check_orders.py
@@ -12,7 +12,7 @@ def orders_status(ids=[]):
         ods = ex.eapiPrivateGetOpenOrders()
         df = pd.DataFrame.from_records(ods)
         #df['dt'] = (tnow - df.updateTime.apply(int))/1000
-        df = df[['status','orderId','symbol','side','price','avgPrice','quantity','executedQty','updateTime','source','clientOrderId','priceScale','quantityScale']]
+        df = df[['status','orderId','symbol','side','price','avgPrice','quantity','executedQty','updateTime','source','priceScale','quantityScale']]
         df['datetime'] = df.updateTime.apply(int).apply(lambda v: datetime.datetime.fromtimestamp(v/1000))
         df = df.sort_values('updateTime', ascending=False)
         print(tabulate(df,headers="keys"))
