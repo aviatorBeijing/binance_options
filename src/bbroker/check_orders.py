b72edeed1b61f70fcc0d0c3449f173e7daf5365a
Jun Ma <junma018@outlook.com>
2024-03-27 10:42:52 +0800

    dev

--- src/bbroker/check_orders.py
+++ src/bbroker/check_orders.py
@@ -19,7 +19,19 @@ def orders_status(ids=[]):
 
 def position_status():
     ods = ex.eapiPrivateGetPosition()
-    print(ods)
+    """
+    'entryPrice': '55.5', 'symbol': 'ETH-240329-3550-P', 'side': 'LONG', 
+    'quantity': '0.1', 'reducibleQty': '0', 'markValue': '4.64', 'ror': '-0.1639', 
+    'unrealizedPNL': '-0.91', 'markPrice': '46.4', 'strikePrice': '3550.00000000', 
+    'positionCost': '5.55', 'expiryDate': '1711699200000', 'priceScale': '1', 
+    'quantityScale': '2', 'optionSide': 'PUT', 'quoteAsset': 'USDT', 'time': '1711500354804'
+    """
+    df = pd.DataFrame.from_records(ods)
+    df = df[['status','orderId','symbol','side','positionCost',
+                'entryPrice','markPrice','quantity','markValue','updateTime','expiryDate']]
+    df['datetime'] = df.time.apply(int).apply(lambda v: datetime.datetime.fromtimestamp(v/1000))
+    df = df.sort_values('time', ascending=False)
+    print(tabulate(df,headers="keys"))
 
 # tests
 if __name__ == '__main__':
