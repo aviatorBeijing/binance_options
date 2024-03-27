4e2aa3e3aa98dd43f07b7874aadeb46b2b923563
Jun Ma <junma018@outlook.com>
2024-03-27 10:32:57 +0800

    dev

--- src/bbroker/check_orders.py
+++ src/bbroker/check_orders.py
@@ -1,16 +1,19 @@
-import os
+import os,datetime
 import pandas as pd
 from tabulate import tabulate
 
 from bbroker.settings import ex
 
 def orders_status(ids=[]):
+    tnow = datetime.datetime.utcnow().timestamp()*1000;tnow=int(tnow)
     if ids:
         pass 
     else: # all
         ods = ex.eapiPrivateGetOpenOrders()
         df = pd.DataFrame.from_records(ods)
         df = df[['status','orderId','symbol','side','price','avgPrice','quantity','executedQty','updateTime','source','clientOrderId','priceScale','quantityScale']]
+        df['dt'] = (tnow - df.updateTime.apply(int))/1000
+        df = df.sort_values('updateTime', ascending=False)
         print(tabulate(df,headers="keys"))
 
 # tests
