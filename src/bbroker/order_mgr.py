e50cc7fe98c7553cd227513c873485faed1d71db
Jun Ma <junma018@outlook.com>
2024-03-27 11:20:18 +0800

    dev

--- src/bbroker/order_mgr.py
+++ src/bbroker/order_mgr.py
@@ -2,17 +2,15 @@ from optparse import check_builtin
 from bbroker.settings import ex
 
 def mgr(symbol,action,qty,pce, timing='limit'):
-    assert action in ['long','short'], f"Not supported action: {action}"
+    assert action in ['buy','sell'], f"Not supported action: {action}"
     assert timing in ['limit','market'], f"Not supported timing: {timing}"
     if symbol.endswith('-C'):
-        #ex.create_order(symbol,'call',action,qty,pce)
-        req = ex.create_order_request(symbol,'call',action,qty,pce,{})
-        ex.eapiPrivatePostOrder( req )
+        ex.create_order(symbol,timing,action,qty,pce)
     elif symbol.endswith('-P'):
         ex.create_order(symbol,'put',action,qty,pce)
 
 def buy_call(symbol,qty,pce):
-    mgr(symbol,'long', qty,pce,timing='limit')
+    mgr(symbol,'buy', qty,pce,timing='limit')
 
 # Test  
 if __name__ == '__main__':
