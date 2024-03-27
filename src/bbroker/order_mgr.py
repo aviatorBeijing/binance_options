5a513358d32f39448e207e31acb6825c579f0b5a
Jun Ma <junma018@outlook.com>
2024-03-27 11:08:04 +0800

    dev

---
+++ src/bbroker/order_mgr.py
@@ -0,0 +1,25 @@
+from optparse import check_builtin
+from bbroker.settings import ex
+
+def mgr(symbol,action,qty,pce, timing='limit'):
+    assert action in ['long','short'], f"Not supported action: {action}"
+    assert timing in ['limit','market'], f"Not supported timing: {timing}"
+    if symbol.endswith('-C'):
+        ex.eapiPrivatePostOrder(symbol,'call',action,qty,pce)
+    elif symbol.endswith('-P'):
+        ex.eapiPrivatePostOrder(symbol,'put',action,qty,pce)
+
+def buy_call(symbol,qty,pce):
+    mgr(symbol,'long', qty,pce,timing='limit')
+
+# Test  
+if __name__ == '__main__':
+    from bbroker.check_status import orders_status
+
+    symbol = 'BTC-240329-24000-C'
+    buy_call(symbol, 0.01, 10)
+
+    import time
+    while True:
+        orders_status()
+        time.sleep(5)
