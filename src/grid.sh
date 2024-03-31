6e3b0c766f2cd6192327514009a66bedc4c61d94
Jun Ma <junma018@outlook.com>
2024-03-31 15:27:42 +0800

    dev

--- src/grid.sh
+++ src/grid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 $PYTHON strategy/grid_trading.py --ric DOGE/USDT --nominal=10000 --stop_loss=-0.2 
+$PYTHON strategy/grid_trading.py --ric SOL/USDT --nominal=1 --stop_loss=-0.2
 $PYTHON strategy/grid_trading.py --ric ETH/USDT --nominal=1 --stop_loss=-0.2
 $PYTHON strategy/grid_trading.py --ric BTC/USDT --nominal=1 --stop_loss=-0.2 
