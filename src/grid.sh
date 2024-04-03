e3f2ab874b8b1b25f598c1039ae72d6c9b7d0f7e
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-04-03 08:32:56 +0800

    dev

--- src/grid.sh
+++ src/grid.sh
@@ -4,4 +4,5 @@ SPANS=$1
 $PYTHON strategy/grid_trading.py --ric DOGE/USDT --nominal=10000 --stop_loss=-0.2 --spans=$SPANS 
 $PYTHON strategy/grid_trading.py --ric SOL/USDT --nominal=100 --stop_loss=-0.2 --spans=$SPANS
 $PYTHON strategy/grid_trading.py --ric ETH/USDT --nominal=1 --stop_loss=-0.2 --spans=$SPANS
+$PYTHON strategy/grid_trading.py --ric LTC/USDT --nominal=100 --stop_loss=-0.2 --spans=$SPANS
 $PYTHON strategy/grid_trading.py --ric BTC/USDT --nominal=1 --stop_loss=-0.2  --spans=$SPANS
