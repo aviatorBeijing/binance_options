436f8df517fede96f1030366dabe4b9409b173b1
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-04-08 19:45:47 +0800

    script

--- src/btc_straddle.sh
+++ src/btc_straddle.sh
@@ -3,10 +3,10 @@
 # can also use "./atms.sh" to find all avalialbe ATM options, and select two ATM options.
 
 CRYPTO=BTC
-DT=240408
+DT=240410
 
-PUT=66250
-CALL=66250
+PUT=71500
+CALL=71500
 
 echo $PYTHON ws_bcontract.py --channel ticker --rics $CRYPTO-$DT-$PUT-P,$CRYPTO-$DT-$CALL-C
 # $PYTHON ws_bcontract.py --channel ticker --rics $CRYPTO-$DT-$PUT-P,$CRYPTO-$DT-$CALL-C
