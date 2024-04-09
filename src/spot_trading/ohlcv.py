f1192342020e0731884aa945cc9f5383b04ed6df
Jun Ma <junma018@outlook.com>
2024-04-09 20:05:06 +0800

    dev

--- src/spot_trading/ohlcv.py
+++ src/spot_trading/ohlcv.py
@@ -36,12 +36,14 @@ async def ohlcv(data):
 def orders_status(ex,ric)->pd.DataFrame:
     #tnow = datetime.datetime.utcnow().timestamp()*1000;tnow=int(tnow)
     ods = ex.fetchOpenOrders(ric)
+    ods = list(map(lambda e: e['info'],ods))
     df = pd.DataFrame.from_records(ods)
     if df.empty: 
         print('*** No outstanding orders.')
         return pd.DataFrame()
+    print(df.columns)
     #df['dt'] = (tnow - df.updateTime.apply(int))/1000
-    df = df[['status','orderId','symbol','side','price','avgPrice','quantity','executedQty','updateTime','source','priceScale','quantityScale']]
+    #df = df[['status','orderId','symbol','side','price','avgPrice','quantity','executedQty','updateTime','source','priceScale','quantityScale']]
     df['datetime'] = df.updateTime.apply(int).apply(lambda v: datetime.datetime.fromtimestamp(v/1000))
     df = df.sort_values('updateTime', ascending=False)
     print('--[ orders ]\n',tabulate(df,headers="keys"))
