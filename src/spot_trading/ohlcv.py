700605d0aa3392b32d8739ed311d299cc5ad0621
Jun Ma <junma018@outlook.com>
2024-04-09 18:22:24 +0800

    dev

--- src/spot_trading/ohlcv.py
+++ src/spot_trading/ohlcv.py
@@ -10,30 +10,26 @@ from cryptofeed.defines import TRADES
 from cryptofeed.exchanges import Binance
 
 WINDOW_IN_SECONDS = 5
-
-doge_volume_stacks = []
-doge_volume_stacks_len=10*12 # Working with WINDOW_IN_SECONDS,  defines the length of history
+stacks_len=10*12 # Working with WINDOW_IN_SECONDS,  defines the length of history
 rows = []
+
 async def ohlcv(data):
-    global doge_volume_stacks
-    global doge_volume_stacks_len
+    global stacks_len
     global rows 
     for k,v in data.items():
         vv = v;v['ric']=k
-        rows+= [ vv ]
-        if len(rows) > doge_volume_stacks_len:
-            rows = rows[ -doge_volume_stacks_len:]
         if 'DOGE' in k:
-            doge_volume_stacks += [ v['volume'] ]
-            l = doge_volume_stacks_len
-            if len(doge_volume_stacks)> l :
-                doge_volume_stacks = doge_volume_stacks[-l:] # pop front
+            rows+= [ vv ]
+        if len(rows) > stacks_len:
+            rows = rows[ -stacks_len:]
 
     df = pd.DataFrame.from_records( rows )
     print(tabulate(df,headers="keys"))
-    if len(doge_volume_stacks)>3:
-        vrk = scipy.stats.percentileofscore( doge_volume_stacks, doge_volume_stacks[-1] )
-        print( len(doge_volume_stacks), vrk )
+
+    volumes = df.volume.values
+    if len(volumes)>3:
+        vrk = scipy.stats.percentileofscore( volumes, volumes[-1] )
+        print( len(volumes), vrk )
 
 @click.command()
 @click.option('--rics',default="BTC-USDT")
