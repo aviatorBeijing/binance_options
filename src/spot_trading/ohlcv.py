5b6f4046171e8f43d13551624405e4f1d157fb91
Jun Ma <junma018@outlook.com>
2024-04-09 19:55:56 +0800

    dev

--- src/spot_trading/ohlcv.py
+++ src/spot_trading/ohlcv.py
@@ -9,6 +9,8 @@ from cryptofeed.backends.aggregate import OHLCV
 from cryptofeed.defines import TRADES
 from cryptofeed.exchanges import Binance
 
+from bbroker.settings import spot_ex
+
 WINDOW_IN_SECONDS = 5
 stacks_len=10*12 # Working with WINDOW_IN_SECONDS,  defines the length of history
 rows = []
@@ -31,9 +33,24 @@ async def ohlcv(data):
         vrk = scipy.stats.percentileofscore( volumes, volumes[-1] )
         print( len(volumes), vrk )
 
+def orders_status(ex)->pd.DataFrame:
+    #tnow = datetime.datetime.utcnow().timestamp()*1000;tnow=int(tnow)
+    ods = ex.eapiPrivateGetOpenOrders()
+    df = pd.DataFrame.from_records(ods)
+    if df.empty: 
+        print('*** No outstanding orders.')
+        return pd.DataFrame()
+    #df['dt'] = (tnow - df.updateTime.apply(int))/1000
+    df = df[['status','orderId','symbol','side','price','avgPrice','quantity','executedQty','updateTime','source','priceScale','quantityScale']]
+    df['datetime'] = df.updateTime.apply(int).apply(lambda v: datetime.datetime.fromtimestamp(v/1000))
+    df = df.sort_values('updateTime', ascending=False)
+    print('--[ orders ]\n',tabulate(df,headers="keys"))
+    return df
+
 @click.command()
 @click.option('--rics',default="BTC-USDT")
 def main(rics):
+    orders_status( spot_ex )
     f = FeedHandler()
     #f.add_feed(Coinbase(symbols=['BTC-USD', 'ETH-USD', 'BCH-USD'], channels=[TRADES], callbacks={TRADES: OHLCV(ohlcv, window=10)}))
     f.add_feed(Binance(symbols=rics.split(','),channels=[TRADES], callbacks={TRADES: OHLCV(ohlcv, window=WINDOW_IN_SECONDS)}))
