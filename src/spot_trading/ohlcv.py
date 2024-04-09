7a8bff728942a1e0c9404b1a970225fce8a513a7
Jun Ma <junma018@outlook.com>
2024-04-09 18:19:18 +0800

    dev

--- src/spot_trading/ohlcv.py
+++ src/spot_trading/ohlcv.py
@@ -9,23 +9,27 @@ from cryptofeed.backends.aggregate import OHLCV
 from cryptofeed.defines import TRADES
 from cryptofeed.exchanges import Binance
 
+WINDOW_IN_SECONDS = 5
 
 doge_volume_stacks = []
-doge_volume_stacks_len=10
+doge_volume_stacks_len=10*12 # Working with WINDOW_IN_SECONDS,  defines the length of history
+rows = []
 async def ohlcv(data):
     global doge_volume_stacks
     global doge_volume_stacks_len
-    recs = []
+    global rows 
     for k,v in data.items():
         vv = v;v['ric']=k
-        recs+= [ vv ]
+        rows+= [ vv ]
+        if len(rows) > doge_volume_stacks_len:
+            rows = rows[ -doge_volume_stacks_len:]
         if 'DOGE' in k:
             doge_volume_stacks += [ v['volume'] ]
             l = doge_volume_stacks_len
             if len(doge_volume_stacks)> l :
                 doge_volume_stacks = doge_volume_stacks[-l:] # pop front
 
-    df = pd.DataFrame.from_records( recs )
+    df = pd.DataFrame.from_records( rows )
     print(tabulate(df,headers="keys"))
     if len(doge_volume_stacks)>3:
         vrk = scipy.stats.percentileofscore( doge_volume_stacks, doge_volume_stacks[-1] )
@@ -36,7 +40,7 @@ async def ohlcv(data):
 def main(rics):
     f = FeedHandler()
     #f.add_feed(Coinbase(symbols=['BTC-USD', 'ETH-USD', 'BCH-USD'], channels=[TRADES], callbacks={TRADES: OHLCV(ohlcv, window=10)}))
-    f.add_feed(Binance(symbols=rics.split(','),channels=[TRADES], callbacks={TRADES: OHLCV(ohlcv, window=5)}))
+    f.add_feed(Binance(symbols=rics.split(','),channels=[TRADES], callbacks={TRADES: OHLCV(ohlcv, window=WINDOW_IN_SECONDS)}))
 
     f.run()
 
