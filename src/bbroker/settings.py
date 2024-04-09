d95bb2c839d7aa8852fc7f99925991ecfd8e5d54
Jun Ma <junma018@outlook.com>
2024-04-09 20:40:49 +0800

    dev

--- src/bbroker/settings.py
+++ src/bbroker/settings.py
@@ -55,6 +55,6 @@ class BianceSpot:
         df['side'] = df.isBuyer.apply(lambda v: 'SELL' if not v else "BUY")
         df = df['id,symbol,side,qty,price,commission,commissionAsset,isMaker,time'.split(',')]
         df['datetime'] = df.time.apply(int).apply(lambda v: datetime.datetime.fromtimestamp(v/1000))
-        df = df.sort_values('updateTime', ascending=False)
+        df = df.sort_values('time', ascending=False)
         print('--[ orders ]\n',tabulate(df,headers="keys"))
         return df  
