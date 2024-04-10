1994a87636d9df25e2feec8324f55b59816488c8
Jun Ma <junma018@outlook.com>
2024-04-10 10:26:53 +0800

    dev:spot

--- src/spot_trading/portfolio.py
+++ src/spot_trading/portfolio.py
@@ -37,10 +37,10 @@ def portfolio_check(ric):
 def price_range(ric, span='5m'):
     ohlcv = binance_kline(symbol=ric.replace('-','/'),span=span,grps=1)
     ohlcv = ohlcv.tail( int(2*60/5) )
-    print('-- ', ohlcv.iloc[0].timestamp, '~', ohlcv.iloc[-1].timestamp)
+    print(f'-- [{ohlcv.shape[0]}]', ohlcv.iloc[0].timestamp, '~', ohlcv.iloc[-1].timestamp)
     print(ohlcv)
 
-    print( np.percentile(ohlcv.low,5), np.percentile(ohlcv.high,99), np.max(ohlcv.high) )
+    print( np.percentile(ohlcv.low,5), np.percentile(ohlcv.high,99) )
 
 @click.command()
 @click.option('--ric',default="DOGE-USDT")
