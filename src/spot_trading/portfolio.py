0896e58a31e79bb8f627cb52b7f8884cdbc908a5
Jun Ma <junma018@outlook.com>
2024-04-10 09:39:31 +0800

    dev:spot

--- src/spot_trading/portfolio.py
+++ src/spot_trading/portfolio.py
@@ -36,7 +36,7 @@ def portfolio_check(ric):
 
 def price_range(ric, span='5m'):
     ohlcv = binance_kline(symbol=ric.replace('-','/'),span=span,grps=1)
-    ohlcv = ohlcv.tail( int(8*60/5) )
+    ohlcv = ohlcv.tail( int(4*60/5) )
     print('-- ', ohlcv.iloc[0].timestamp, '~', ohlcv.iloc[-1].timestamp)
     print(ohlcv)
 
