e7d3e98bf93563a4fb5fee312d6bf60286dc711f
Jun Ma <junma018@outlook.com>
2024-04-13 13:08:20 +0800

    dev:spot

--- src/fetch_binance.sh
+++ src/fetch_binance.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 scp -i ~/.ssh/junma-japan.pem ubuntu@3.114.152.67:/home/ubuntu/tmp/binance_*.csv ./
+scp -i ~/.ssh/junma-japan.pem ubuntu@3.114.152.67:/home/ubuntu/tmp/binance_fee_gain.dat ./
