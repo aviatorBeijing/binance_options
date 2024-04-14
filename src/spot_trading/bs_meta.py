d29cfbb40bdb720eef419e9bf6ae872a7334d1a8
Jun Ma <junma018@outlook.com>
2024-04-14 15:28:52 +0800

    dev:spot

--- src/spot_trading/bs_meta.py
+++ src/spot_trading/bs_meta.py
@@ -116,7 +116,8 @@ def main(cbuy,csell,cancel,price,qty):
     ex = BianceSpot('DOGE/USDT', spot_ex=spot_ex)
     
     if cancel:
-        ex.cancel_order( cancel.split(',') )
+        for oid in cancel.split(','):
+            ex.cancel_order( oid )
     else:
         price = float(price)
         qty = float(qty)
