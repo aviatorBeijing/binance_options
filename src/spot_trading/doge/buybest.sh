cd24c264b345b3349910907bbaaef7aad62344cd
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-04-15 10:01:21 +0800

    dev: option pricing

---
+++ src/spot_trading/doge/buybest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$PYTHON spot_trading/bs_meta.py --ric DOGE/USDT --buybest --qty=$1
---
+++ src/spot_trading/doge/sell.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$PYTHON spot_trading/bs_meta.py --csell --ric=DOGE/USDT --price $1 --qty $2
