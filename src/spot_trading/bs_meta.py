8f1eed19fa062b0fb11ec29d6787ec3106fc4c10
Jun Ma <junma018@outlook.com>
2024-04-15 16:24:12 +0800

    dev:spot

--- src/spot_trading/bs_meta.py
+++ src/spot_trading/bs_meta.py
@@ -162,7 +162,7 @@ def main(ric, cbuy,csell,cancel,price,qty,sellbest,buybest,centered_aligned_pair
         assert qty>0, 'Must provide a qty>0'
         bid,ask = get_spot_(ex)
         pce = (bid+ask)*.5
-        e = 5/100.
+        e = 5/1000.
         ex.buy( pce*(1-e), qty, ask )
         ex.sell(pce*(1+e), qty, bid)
     elif sellbest or buybest:
