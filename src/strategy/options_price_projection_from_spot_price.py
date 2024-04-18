ebaf6d68913ad1887b633a90198105035589e2b8
Jun Ma <junma018@outlook.com>
2024-04-18 19:44:19 +0800

    dev:spot

--- src/strategy/options_price_projection_from_spot_price.py
+++ src/strategy/options_price_projection_from_spot_price.py
@@ -15,36 +15,38 @@ from strategy.delta_gamma import callprice,putprice
 from butil.options_calculator import extract_specs, callprice, putprice, invert_callprice, invert_putprice
 
 
-def _main(contracts, reference_spot):
+def _main(contracts:list, reference_spots:list):
     """
     @brief 
     """
-    S = reference_spot
     underlying = get_underlying( contracts[0])
     spot_now, _ = get_binance_spot(underlying)
     recs = [] 
-    for contract in contracts:
-        _, T, K, ctype = extract_specs(contract)
-        cdata = fetch_bidask(contract.upper())
-        ask = float(cdata['ask'])
-        sigma = float(cdata['impvol_ask'])
-                
-        func_ = None
-        if ctype == 'call':
-            func_ = callprice
-        elif ctype == 'put':
-            func_ = putprice
-        op = func_(S,K,T/365,sigma,0.)
-        recs += [ {'contract': contract, 'spot': S, 'option': op} ]
-    df = pd.DataFrame.from_records( recs )
-    print( tabulate(df, headers="keys"))
+    for S in reference_spots:
+        for contract in contracts:
+            _, T, K, ctype = extract_specs(contract)
+            cdata = fetch_bidask(contract.upper())
+            ask = float(cdata['ask'])
+            sigma = float(cdata['impvol_ask'])
 
-def _multiprocess_main(contracts:list,projected_spot_price:float):
+            func_ = None
+            if ctype == 'call':
+                func_ = callprice
+            elif ctype == 'put':
+                func_ = putprice
+            op = func_(S,K,T/365,sigma,0.)
+            recs += [ {'contract': contract, 'spot': S, 'option': op} ]
+    df = pd.DataFrame.from_records( recs )
+    df.sort_values('spot', ascending=False, inplace=True)
+    print( tabulate(df[df.contract.str.contains('-C')], headers="keys"))
+    print( tabulate(df[df.contract.str.contains('-P')], headers="keys"))
+
+def _multiprocess_main(contracts:list,projected_spot_prices:list):
     print('-- waiting data...')
     time.sleep(2)
     while True:
         try:
-            _main( contracts, projected_spot_price)
+            _main( contracts, projected_spot_prices)
             time.sleep(5)
         except KeyboardInterrupt as e:
             print("-- Exiting --")
@@ -52,11 +54,12 @@ def _multiprocess_main(contracts:list,projected_spot_price:float):
 
 @click.command()
 @click.option('--contracts', help="contract name")
-@click.option('--projected_spot_price')
-def main(contracts,projected_spot_price):
-
+@click.option('--projected_spot_prices')
+def main(contracts,projected_spot_prices):
+    projected_spot_prices = list(map(lambda s: float(s), projected_spot_prices.split(',')))
+    
     conn = Process( target=ws_connector, args=(f"{contracts}", "ticker",) )
-    calc = Process( target=_multiprocess_main, args=(contracts.split(','), float(projected_spot_price)) )
+    calc = Process( target=_multiprocess_main, args=(contracts.split(','), projected_spot_prices) )
     conn.start()
     calc.start()
     
