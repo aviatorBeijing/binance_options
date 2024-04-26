570d7a998dbba336a63634522632811004a001f4
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-04-26 10:47:12 +0800

    dev:spot

--- src/daily_returns_stats.sh
+++ src/daily_returns_stats.sh
@@ -4,3 +4,5 @@ source setup.sh
 $PYTHON sentiments/spot_vol.py --rics=btc
 $PYTHON sentiments/spot_vol.py --rics=eth
 $PYTHON sentiments/spot_vol.py --rics=doge
+$PYTHON sentiments/spot_vol.py --rics=pendle
+$PYTHON sentiments/spot_vol.py --rics=sol
--- src/spot_trading/doge/instant_pair.sh
+++ src/spot_trading/doge/instant_pair.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$PYTHON spot_trading/bs_meta.py --centered_pair  --centered_pair_dist=25 --qty $1 --ric DOGE/USDT
+$PYTHON spot_trading/bs_meta.py --centered_pair  --qty $1 --ric DOGE/USDT --centered_pair_dist=25
 $PYTHON spot_trading/portfolio.py
