91161565a809208c523aae86f748a2cb33221d21
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-05-08 22:46:22 +0800

    dev:spot

--- src/p.sh
+++ src/p.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-source /home/ubuntu/.bashrc
-export PYTHONPATH=$pwd:$PYTHONPATH
-/usr/bin/python3 spot_trading/portfolio.py
-
-$PYTHON perp_trading/perp_meta.py --check
+$PYTHON spot_trading/portfolio.py
---
+++ src/perp_trading/doge/buybest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+$PYTHON perp_trading/perp_meta.py --ric DOGE/USDT --buybest --qty=$1
