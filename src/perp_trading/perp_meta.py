3579e5c9de340ae15ad2768470e99e23b1346873
Jun Ma <junma018@outlook.com>
2024-05-09 12:16:13 +0800

    dev:spot

--- src/perp_trading/perp_meta.py
+++ src/perp_trading/perp_meta.py
@@ -314,6 +314,8 @@ def adhoc_ticker(symbol='BTC/USDT')->tuple:
     #qts = perp_ex.fetch_ticker(symbol)
     qts = perp_ex.public_get_ticker_bookticker({'symbol': symbol.replace('/','').replace('-','').upper()})
     bid,ask = qts['bidPrice'],qts['askPrice'] # bidQty,askQty
+    bid = float(bid)
+    ask = float(ask)
 
     spread = (ask-bid)/(ask+bid)*2
     assert spread< 5./10_000, f'spread is too wide: {spread} (bid:{bid},ask:{ask})'
