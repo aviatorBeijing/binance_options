f87a30a76525e1acbd1f35284e68967a07865275
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-05-10 20:49:39 +0800

    dev:spot

--- src/spot_trading/doge/instant_pair.sh
+++ src/spot_trading/doge/instant_pair.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$PYTHON spot_trading/bs_meta.py --centered_pair  --qty $1 --ric DOGE/USDT --centered_pair_dist=35
+$PYTHON spot_trading/bs_meta.py --centered_pair  --qty $1 --ric DOGE/USDT --centered_pair_dist=25
 $PYTHON spot_trading/portfolio.py
