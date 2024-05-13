011f8793a21c68a29ae739f98dc54238af578405
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-05-13 19:21:51 +0800

    dev: perp

--- src/mkt_spot.sh
+++ src/mkt_spot.sh
@@ -4,3 +4,6 @@ RIC=DOGE/USDT
 
 $PYTHON spot_trading/market_data.py --ric $RIC --span 5m
 $PYTHON spot_trading/market_data.py --ric $RIC --span 1h
+
+$PYTHON perp_trading/marketdata.py  --ric $RIC --span 5m
+$PYTHON perp_trading/marketdata.py  --ric $RIC --span 1h
