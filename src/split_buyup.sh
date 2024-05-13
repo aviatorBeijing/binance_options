5da8236870a70301d2f797b8c2b2436f0cb49ff4
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-05-13 20:30:52 +0800

    dev: perp

--- src/split_buyup.sh
+++ src/split_buyup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$PYTHON spot_trading/bs_meta.py --buyup_split configs/split_config_buyup.yml --ric DOGE/USDT
+$PYTHON spot_trading/bs_meta.py --buyup_split examples/configs/split_config_buyup.yml --ric DOGE/USDT
--- src/split_selldown.sh
+++ src/split_selldown.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$PYTHON spot_trading/bs_meta.py --selldown_split configs/split_config_selldown.yml --ric DOGE/USDT
+$PYTHON spot_trading/bs_meta.py --selldown_split examples/configs/split_config_selldown.yml --ric DOGE/USDT
