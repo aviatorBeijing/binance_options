5f938a64c329998466ad9d73f0cb3eda53b1e6a3
Jun Ma <junma018@outlook.com>
2024-05-16 10:39:21 +0800

    dev: perp

--- src/butil/bsql.py
+++ src/butil/bsql.py
@@ -65,7 +65,7 @@ def fetch_bidask(contract) ->dict:
         t0 = d['ts_beijing']
         t1 = datetime.datetime.utcnow() +datetime.timedelta(hours=8)
         t1 = t1.timestamp()
-        assert (t1-t0)<10, f'{contract}, data latency = {t1-t0} secs. Websocket data pipe is NOT alive.'
+        assert (t1-t0)<10, f'{contract}, data latency = {t1-t0} secs. Websocket data pipe is NOT alive or market is stalling.'
         
     cols = ["last_trade","bid","ask","bidv","askv","delta","gamma","theta",
                         "vega","impvol","impvol_bid","impvol_ask",
