f2558c9d8537e71380d918c5823f3f91f443ae1d
Jun Ma <junma018@outlook.com>
2024-05-17 23:55:04 +0800

    dev: perp

--- src/arb/perp_spot_arg.py
+++ src/arb/perp_spot_arg.py
@@ -1,10 +1,13 @@
+from email.policy import default
 import os,datetime,click 
 import pandas as pd 
 
 from butil.butils import get_binance_next_funding_rate
 
-def main():
-    ric = 'ONT/USDT'
+@click.command()
+@click.option('--ric', default='BTC/USDT')
+def main(ric):
+    ric = ric.upper()
     apr, r, ts = get_binance_next_funding_rate(ric)
     print( apr, r, ts)
 
