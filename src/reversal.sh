d928832aa17034770c81e9fb08644e1f56ef2ee9
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-06-02 21:22:19 +0800

    dev:spot

--- src/reversal.sh
+++ src/reversal.sh
@@ -2,7 +2,7 @@
 
 for s in BTC ETH BNB SOL XRP ADA AVAX LINK DOT TRX;do
        echo
-       #$PYTHON spot_trading/market_data.py --ric $s/USDT --span 1d	
+       $PYTHON spot_trading/market_data.py --ric $s/USDT --span 1d	
 done
 
 $PYTHON signals/reversal_from_longterm_low.py --syms btc,eth,bnb,sol,xrp,ada,avax,link,dot,trx --volt $1
