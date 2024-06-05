2beef1dfd54da0b3d8ee44d4b1061e874e8f9794
Jun Ma <junma018@outlook.com>
2024-06-05 15:51:17 +0800

    dev:spot

--- src/reversal_data.sh
+++ src/reversal_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SERVER=3.114.152.67
-for s in btc eth bnb sol xrp ada avax link dot trx;do
-   scp -i ~/.ssh/junma-japan.pem ubuntu@$SERVER:/home/ubuntu/tmp/${s}-usdt_1d.csv ./ && mv ${s}-usdt_1d.csv ~/tmp
-done
-
+scp -i ~/.ssh/junma-japan.pem ubuntu@$SERVER:/home/ubuntu/tmp/reversal_data.tar.gz ./ && mv reversal_data.tar.gz ~/tmp
+foodir=`pwd`
+cd ~/tmp
+tar xvfz reversal_data.tar.gz
+cd $foodir
