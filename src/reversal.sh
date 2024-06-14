d7411b6b0143fcfdc43e66ad53a33ef0444b48a7
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-06-14 14:18:41 +0800

    dev:spot

--- src/reversal.sh
+++ src/reversal.sh
@@ -3,7 +3,7 @@
 arr=()
 syms=()
 datafiles=()
-for s in BTC ETH BNB SOL XRP ADA AVAX LINK DOT TRX LTC FTM BCH MATIC ARB FIL OP ATOM PEPE;do
+for s in BTC DOGE ETH BNB SOL XRP ADA AVAX LINK DOT TRX LTC FTM BCH MATIC ARB FIL OP ATOM PEPE;do
 	arr+=( "${s}/USDT" )
 	syms+=( ${s,,} )
 	datafiles+=( ${s,,}-usdt_1d.csv )
