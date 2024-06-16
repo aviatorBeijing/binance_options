2f331ec6cd10736f68d0186675ae00fca66d7bdc
Jun Ma <junma018@outlook.com>
2024-06-16 21:39:56 +0800

    dev:spot

---
+++ src/var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+$PYTHON spot_trading/portfolio.py --hedging
+
+$PYTHON spot_trading/portfolio.py --hedging --var_cryptos sei,ace,pendle
+
+$PYTHON spot_trading/portfolio.py --hedging --var_cryptos sol,btc,doge,bnb,eth,usdt
