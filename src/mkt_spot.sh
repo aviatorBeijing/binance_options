591882ee91d922d04e553a458da65a3f72c1d6f9
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-06-23 17:53:57 +0800

    dev:spot

--- src/mkt_spot.sh
+++ src/mkt_spot.sh
@@ -22,6 +22,14 @@ $PYTHON spot_trading/market_data.py --ric $RIC --span 1d
 $PYTHON perp_trading/marketdata.py  --ric $RIC --span 1h
 $PYTHON perp_trading/marketdata.py  --ric $RIC --span 1d
 
+./bal.sh
 ./reversal.sh 60
 ./kline_spot.sh
 ./gain_fee.sh
+
+PWD=`pwd`
+cd ~/tmp
+tar cvfz binance_ot.tar.gz binance*usdt.csv
+tar cvfz binance_kline.tar.gz *_1d.csv *_1h.csv
+
+cd $PWD
