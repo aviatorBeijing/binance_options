7a6286ba4786fce16b77906149524336be8156f1
Jun Ma <junma018@outlook.com>
2024-08-08 22:23:31 +0800

    dev:spot

--- src/win_losses.sh
+++ src/win_losses.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for s in sol pendle ace sei avax trx btc matic xrp doge;do
+for s in sol pendle ace sei avax trx btc matic xrp doge bnb;do
     $PYTHON spot_trading/portfolio.py --check_cached --ric $s-usdt &
 done
