63e4d18f4f0e87c99f1a47ae20becadb4ae3eea5
Jun Ma <junma018@outlook.com>
2024-09-01 13:41:05 +0800

    dev

--- src/strategy/gamma_scalping/merton_jump_model.py
+++ src/strategy/gamma_scalping/merton_jump_model.py
@@ -8,7 +8,8 @@ plt.style.use('fivethirtyeight')
 
 def read_prices_from_csv(filename):
     data = pd.read_csv(filename)
-    return data['close'].values
+    dates = pd.date_range( end=data['timestamp'].iloc[-1],periods=data.shape[0] )
+    return data['close'].values, dates 
 
 # Negative log-likelihood function for MLE using Merton Jump Diffusion Model
 def negative_log_likelihood(params, log_returns, dt):
@@ -19,21 +20,35 @@ def negative_log_likelihood(params, log_returns, dt):
     for k in range(1, 10):  # Consider up to 10 jumps in one step
         log_jumps = norm.logpdf(log_returns - k * mu_j, loc=(mu - k * lambda_ * mu_j) * dt, scale=np.sqrt(sigma**2 * dt + k * sigma_j**2))
         jumps += poisson.pmf(k, lambda_ * dt) * log_jumps
+    
+    # FIXME: nan values found in "jumps"
+    #for v in jumps:
+    #    if np.isnan(v): print( v )
+
     nll = -np.sum(np.log(jumps))
     return nll
 
 # Function to find the MLE estimates for Merton Jump Diffusion Model
 def find_mle(log_returns, dt):
-    # Initial guesses 
-    initial_guess = [np.mean(log_returns) / dt, np.std(log_returns) / np.sqrt(dt), 0.1, 0, 0.01]
-    
+    initial_guess = [np.mean(log_returns) / dt, np.std(log_returns) / np.sqrt(dt), 0.1, 0, 0.01]    # Initial guesses 
     bounds = [(-np.inf, np.inf), (1e-8, np.inf), (1e-8, np.inf), (-np.inf, np.inf), (1e-8, np.inf)]
     
     # Minimize the negative log-likelihood
-    result = minimize(negative_log_likelihood, initial_guess, args=(log_returns, dt),
-                      bounds=bounds, method='L-BFGS-B')
+    result = minimize(  negative_log_likelihood, 
+                        initial_guess, 
+                        args=(log_returns, dt),
+                        bounds=bounds, 
+                        method='L-BFGS-B')
     
     mu_mle, sigma_mle, lambda_mle, mu_j_mle, sigma_j_mle = result.x
+
+    print('-- MLE results:')
+    print(f"\tEstimated mu: {mu_mle}")
+    print(f"\tEstimated sigma: {sigma_mle}")
+    print(f"\tEstimated lambda (jump intensity): {lambda_mle}")
+    print(f"\tEstimated mu_j (mean jump size): {mu_j_mle}")
+    print(f"\tEstimated sigma_j (jump volatility): {sigma_j_mle}")
+
     return mu_mle, sigma_mle, lambda_mle, mu_j_mle, sigma_j_mle
 
 # Merton Jump Diffusion Model
@@ -56,46 +71,59 @@ def calculate_annualized_volatility(paths, dt):
     volatilities = np.std(log_returns, axis=1) / np.sqrt(dt) 
     return volatilities
 
-def main():
-    prices = read_prices_from_csv(os.getenv('USER_HOME','') + "/tmp/btc-usdt_1d.csv")[-365*5:]
-    log_returns = np.log(prices[1:] / prices[:-1])
-    
-    nDays = 365 # Trading days per year
+def calc_log_returns(prices):
+    return np.log(prices[1:] / prices[:-1])
 
-    # Time step size
-    dt = 1/nDays  # indicates "daily"
-    
+
+nDays = 365 # Trading days per year
+
+def calibrate_and_generate(prices, n_paths=100, horizon=1, t0:str=''):
+    T = horizon  # Time horizon (1 year)
+
     # Calibration
-    mu_mle, sigma_mle, lambda_mle, mu_j_mle, sigma_j_mle = find_mle(log_returns, dt)
+    dt = 1/nDays  # Time step size
+    lrtns = calc_log_returns(prices)
+    mu_mle, sigma_mle, lambda_mle, mu_j_mle, sigma_j_mle = find_mle( lrtns, dt)
 
-    print(f"Estimated mu: {mu_mle}")
-    print(f"Estimated sigma: {sigma_mle}")
-    print(f"Estimated lambda (jump intensity): {lambda_mle}")
-    print(f"Estimated mu_j (mean jump size): {mu_j_mle}")
-    print(f"Estimated sigma_j (jump volatility): {sigma_j_mle}")
-
-    # Simulate 10 paths based on the MLE results
     S0 = prices[-1]  # Starting price (last available price)
-    T = 1  # Time horizon (1 year)
     n_steps = nDays * T
-    n_paths = 100  # Number of paths to simulate
 
     paths = simulate_jump_diffusion_paths(S0, mu_mle, sigma_mle, lambda_mle, mu_j_mle, sigma_j_mle, T, n_steps, n_paths)
+    dates = list( range( n_steps ) )
+    if t0:
+        dates = pd.date_range(start=t0, periods=n_steps+1 )
+    return paths, dates
+
+def main():
+    x = 5
+    prices, dates = read_prices_from_csv(os.getenv('USER_HOME','') + "/tmp/btc-usdt_1d.csv")
+    prices = prices[-nDays*x:]
+    dates  = dates[-nDays*x:]
+    
+    n_paths = 100
+    dt = 1/nDays  # Time step size
+    paths, gen_dates = calibrate_and_generate(prices, n_paths=n_paths, t0=str(dates[-1]) )
     volatilities = calculate_annualized_volatility(paths, dt)
 
     mean_volatility = np.mean(volatilities)
     std_volatility = np.std(volatilities)
+    p68 = np.percentile(volatilities,68)
+    print('\t', mean_volatility, std_volatility, p68)
 
-    plt.figure(figsize=(18, 12))
+    plt.figure(figsize=(18, 20))
     
-    plt.subplot(2,1,1)
+    plt.subplot(3,1,1)
+    plt.plot(dates, prices)
+    plt.title(f'Market Prices ({x}-Yr)')
+
+    plt.subplot(3,1,2)
     for i in range(n_paths):
-        plt.plot(paths[:, i], lw=1.5)
+        plt.plot(gen_dates, paths[:, i], lw=1.5)
     plt.title(f'Merton Jump Diffusion Model (n={n_paths})')
     plt.xlabel('Time Steps (Days)')
     plt.ylabel('Price')
 
-    plt.subplot(2,1,2)
+    plt.subplot(3,1,3)
     plt.plot(volatilities, lw=2, color='blue')
     plt.axhline(mean_volatility + std_volatility, color='red', linestyle='--', label='+1σ Bound')
     plt.axhline(mean_volatility - std_volatility, color='green', linestyle='--', label='-1σ Bound')
