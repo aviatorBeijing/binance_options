dd329987a2ab68e6bfd38e1b1d69043fa1d3f92b
Ubuntu <ubuntu@ip-172-31-46-0.ap-northeast-1.compute.internal>
2024-09-15 20:39:51 +0800

    dev

--- src/strategy/options_pricing/app.sh
+++ src/strategy/options_pricing/app.sh
@@ -4,4 +4,5 @@ PCS=$2
 CONTRACTS=$1
 
 export PYTHONPATH=/home/ubuntu/src/binance_options/src:$PYTHONPATH
+cd /home/ubuntu/src/binance_options/src/strategy/options_pricing
 $PYTHON options_price_projection_from_spot_price.py --contracts $CONTRACTS --projected_spot_prices $PCS
