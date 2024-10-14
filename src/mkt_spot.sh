5c50ddb6369514a7b5cba76f08f6631365da575a
Jun Ma <junma018@outlook.com>
2024-10-14 08:51:14 +0800

    dev

--- src/mkt_spot.sh
+++ src/mkt_spot.sh
@@ -15,13 +15,6 @@ $PYTHON spot_trading/market_data.py --ric $RIC --span 1d
 $PYTHON perp_trading/marketdata.py  --ric $RIC --span 1h
 $PYTHON perp_trading/marketdata.py  --ric $RIC --span 1d
 
-RIC=MATIC/USDT
-$PYTHON spot_trading/market_data.py --ric $RIC --span 1h
-$PYTHON spot_trading/market_data.py --ric $RIC --span 1d
-
-$PYTHON perp_trading/marketdata.py  --ric $RIC --span 1h
-$PYTHON perp_trading/marketdata.py  --ric $RIC --span 1d
-
 RIC=BNB/USDT
 $PYTHON spot_trading/market_data.py --ric $RIC --span 1h
 $PYTHON spot_trading/market_data.py --ric $RIC --span 1d
