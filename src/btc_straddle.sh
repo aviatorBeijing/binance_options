c7cde8087be4db0d53f63ee484706bc0a421de98
Jun Ma <junma018@outlook.com>
2024-11-01 16:28:50 +0800

    dev: script

--- src/btc_straddle.sh
+++ src/btc_straddle.sh
@@ -3,13 +3,11 @@
 # can also use "./atms.sh" to find all avalialbe ATM options, and select two ATM options.
 
 CRYPTO=BTC
-DT=240515
+DT=241108
 
-PUT=62250
-CALL=62250
+PUT=70000
+CALL=70000
 
 echo $PYTHON ws_bcontract.py --channel ticker --rics $CRYPTO-$DT-$PUT-P,$CRYPTO-$DT-$CALL-C
-# $PYTHON ws_bcontract.py --channel ticker --rics $CRYPTO-$DT-$PUT-P,$CRYPTO-$DT-$CALL-C
 
-$PYTHON strategy/straddle.py --left $CRYPTO-$DT-$PUT-P --right $CRYPTO-$DT-$CALL-C --size=0.01 --user_premium=16.9 #--check_parity 
-# --user_premium=24.55 
+$PYTHON strategy/straddle.py --left $CRYPTO-$DT-$PUT-P --right $CRYPTO-$DT-$CALL-C --size=0.01 #--user_premium=16.9 #--check_parity 
