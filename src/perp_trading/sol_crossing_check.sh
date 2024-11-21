509928cd85bdedcd98933a4f5603843aa5e3b41b
Jun Ma <junma018@outlook.com>
2024-11-21 13:59:14 +0800

    dev

--- src/perp_trading/sol_crossing_check.sh
+++ src/perp_trading/sol_crossing_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ..
+export PYTHONPATH=$PYTHONPATH:$USER_HOME/src/binance_options/src
+
 RIC=SOL/USDT
 $PYTHON perp_trading/marketdata.py  --ric $RIC --span 15m
 $PYTHON perp_trading/sol_crossing_check.py 
