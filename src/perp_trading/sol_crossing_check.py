1be1c00cdd1ccab9bfa83a477f2f0eb935ce5ec9
Jun Ma <junma018@outlook.com>
2024-11-21 13:48:40 +0800

    dev

--- src/perp_trading/sol_crossing_check.py
+++ src/perp_trading/sol_crossing_check.py
@@ -1,8 +1,8 @@
-import pandas as pd
+import pandas as pd,os
 
 from crossing_check import calculate_price_cross_counts
 
-df = pd.read_csv('/Users/junma/tmp/perp_solusdt_15m.csv')
+df = pd.read_csv(os.getenv("USER_HOME",'') + '/tmp/perp_solusdt_15m.csv')
 df['timestamp'] = df['starttime'].apply(pd.Timestamp)
 
 result = calculate_price_cross_counts(df, use_weight=True)
