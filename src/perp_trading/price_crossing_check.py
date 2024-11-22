3bc6d1aa02e3e074f8f1e9512f4acbfd5535a496
Jun Ma <junma018@outlook.com>
2024-11-22 09:30:35 +0800

    dev: perp: add cmd line option for price levels.

--- src/perp_trading/price_crossing_check.py
+++ src/perp_trading/price_crossing_check.py
@@ -4,11 +4,12 @@ from crossing_check import calculate_price_cross_counts
 
 @click.command()
 @click.option('--sym', default='sol')
-def main(sym):
+@click.option('--levels',default=10)
+def main(sym,levels):
     df = pd.read_csv(os.getenv("USER_HOME",'') + f'/tmp/perp_{sym.lower()}usdt_15m.csv')
     df['timestamp'] = df['starttime'].apply(pd.Timestamp)
 
-    result = calculate_price_cross_counts(df, w=3, use_weight=False)
+    result = calculate_price_cross_counts(df, w=3, use_weight=False, num_levels=levels)
     print(result)
 
 if __name__ == '__main__':
