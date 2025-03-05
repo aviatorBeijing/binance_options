b7178e01c27d9a849e4afcda1b7aceda2e94880a
Jun Ma <junma018@outlook.com>
2025-03-05 17:00:01 +0800

    dev (options): capital allocation on put/call options grid orders.

--- src/ivsurf/on_spot.py
+++ src/ivsurf/on_spot.py
@@ -7,7 +7,7 @@ from butil.butils import ( DATADIR,DEBUG, get_binance_spot, get_underlying )
 from butil.options_calculator import extract_specs,callprice,putprice
 from brisk.bfee import calc_fee
 
-def opricer( contracts : list):
+def opricer( contracts : list, cap_call: float, cap_put: float):
     """
     @brief Main purpose of this function is to help 
             placing daily adjusted orders for option contracts.
@@ -82,6 +82,32 @@ def opricer( contracts : list):
     df['Mkt_CALL'] = df['Mkt_CALL'].apply(lambda v: round(v/5)*5)
     df['Mkt_PUT']  = df['Mkt_PUT'].apply(lambda v: round(v/5)*5)
     
+    # capital allocation strategy
+    call_cap_limit = cap_call
+    put_cap_limit = cap_put
+    shares = np.array([1,2,6,18]) # Double the sum of all previous orders
+    ttl = np.sum( shares )
+    per_c = (call_cap_limit -10.) / ttl 
+    per_p = (put_cap_limit -10.) / ttl
+    
+    df['Qty_CALL'] = 0
+    df.loc[df.index[-4:], 'Qty_CALL']  = per_c * shares
+    df['Qty_CALL'] /= df['Mkt_CALL']
+    df['Qty_CALL'] = df['Qty_CALL'].apply(lambda v: np.floor(v*100)/100.)
+    df['Cost_CALL($)'] = df['Qty_CALL'] * df['Mkt_CALL']
+
+    df['Qty_PUT'] = 0
+    df.loc[df.index[:4], 'Qty_PUT']  = per_p * shares 
+    df['Qty_PUT'] /= df['Mkt_PUT']
+    df['Qty_PUT'] = df['Qty_PUT'].apply(lambda v: np.floor(v*100)/100.)
+    df['Cost_PUT($)'] = df['Qty_PUT'] * df['Mkt_PUT']
+    
+    ttl_call = (df['Qty_CALL'] * df['Mkt_CALL']).sum()
+    ttl_put  = (df['Qty_PUT'] * df['Mkt_PUT']).sum()
+    assert ttl_call <= call_cap_limit, f'Call: {ttl_call} > {call_cap_limit}, insufficient fund (rounding error?).'
+    assert ttl_put  <= put_cap_limit, f'Put: {ttl_put} > {put_cap_limit}, insufficient fund (rounding error?).'
+    # -end-
+
     df['Spot'] = df.index
     df.loc[ df['Spot']>smid, 'Mkt_CALL' ] = ''
     df.loc[ df['Spot']<smid, 'Mkt_PUT' ] = ''
@@ -93,25 +119,32 @@ def opricer( contracts : list):
     df['moneyness'] = df.dp.apply(abs) < 5/1000
     df.moneyness = df.moneyness.apply(lambda s: '*' if s else '')
     df.dp = df.dp.apply(lambda v: f"{(v*100):.1f}%")
-    df = df[['moneyness', 'CALL', 'dp','Mkt_CALL', 'BS_CALL','c%','p%','BS_PUT','Mkt_PUT','dp','PUT']]
+    df = df[['moneyness', 'CALL', 'dp','Mkt_CALL', 'Qty_CALL', 'Cost_CALL($)', 'BS_CALL','c%','p%','BS_PUT','Mkt_PUT','Qty_PUT','Cost_PUT($)','dp','PUT']]
     for col in ['BS_CALL','BS_PUT']:
         df[col] = df[col].apply(lambda v: f'{v:.2f}')
     for col in ['c%','p%']:
         df[col] = df[col].apply(lambda v: f'{v:.1f}')
+
+    df.index = list(map(lambda v: f'{v:,.0f}', df.index ))
+    new_row = df[['Cap_CALL($)','Cap_PUT($)']].sum()
+    df.loc['Ttl'] = new_row
+    df = df.fillna('_')
 
     print( tabulate(df,headers='keys'))
 
 import click 
 @click.command()
 @click.option("--contracts", help="comma-separated contracts", default='BTC-250228-95000-C,BTC-250228-95000-P')
-def main(contracts):
+@click.option('--cap_call', help='capital limit for call options orders', default=500.)
+@click.option('--cap_put', help='capital limit for call options orders', default=500.)
+def main(contracts,cap_call,cap_put):
     """
     Need invoke WSS first, ex.: 
         <USER_HOME>/binance_options/src/ticker.sh BTC-250228-95000-C,BTC-250228-95000-P
     """
     print('-- contracts:',contracts)
     contracts = contracts.upper().split(',')
-    opricer( contracts)
+    opricer( contracts, cap_call, cap_put)
 
 if __name__ == '__main__':
     main()
