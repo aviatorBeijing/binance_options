8e6fb4712136ec8d98080fa960b75f978d404152
JM_NGINX_TUNNEL <jm_nginx_tunnel@gmail.com>
2026-08-15 12:13:08 +0800

    dev

--- src/btc_straddle.sh
+++ src/btc_straddle.sh
@@ -3,11 +3,11 @@
 # can also use "./atms.sh" to find all avalialbe ATM options, and select two ATM options.
 
 CRYPTO=BTC
-DT=260424
+DT=260828
 
-PUT=75000
-CALL=75000
+PUT=63000
+CALL=63000
 
-echo $PYTHON ws_bcontract.py --channel ticker --rics $CRYPTO-$DT-$PUT-P,$CRYPTO-$DT-$CALL-C
+echo "Start first:" $PYTHON ws_bcontract.py --channel ticker --rics $CRYPTO-$DT-$PUT-P,$CRYPTO-$DT-$CALL-C
 
 $PYTHON strategy/straddle.py --left $CRYPTO-$DT-$PUT-P --right $CRYPTO-$DT-$CALL-C --size=0.01 #--user_premium=16.9 #--check_parity 
